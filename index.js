8ac0b3fc345573f36b213e82c9d3b541fed32dbc
Joel Mercier <joelmercier@MacBook-Pro-de-Joel.local>
2016-01-29 11:28:02 +0100

    added mysql

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var browserify = require('browserify');
 var bodyParser = require('body-parser');
 var React = require('react');
 var jsx = require('node-jsx');
-var mongoose   = require('mongoose');
+// var mongoose   = require('mongoose');
+var mysql      = require('mysql');
+var morgan = require('morgan');
 var app = express();
+app.use(morgan('combined'));
 
 jsx.install();
 
@@ -16,36 +19,37 @@ app.use(bodyParser.json());
 // DATABASE SETUP
 // =============================================================================
 
-var dbURI = 'mongodb://nuntius:nuntius@ds047945.mongolab.com:47945/nuntius'; // connect to  database
+// var dbURI = 'mongodb://nuntius:nuntius@ds047945.mongolab.com:47945/nuntius'; // connect to  database
+//
+// var db = mongoose.connection;
+//
+// db.on('connecting', function() {
+//   console.log('connecting to MongoDB...');
+// });
+//
+// db.on('error', function(error) {
+//   console.error('Error in MongoDb connection: ' + error);
+//   mongoose.disconnect();
+// });
+// db.on('connected', function() {
+//   console.log('MongoDB connected!');
+// });
+// db.once('open', function() {
+//   console.log('MongoDB connection opened!');
+// });
+// db.on('reconnected', function () {
+//   console.log('MongoDB reconnected!');
+// });
+// db.on('disconnected', function() {
+//   console.log('MongoDB disconnected!');
+//   mongoose.connect(dbURI, {server:{auto_reconnect:true}});
+// });
+// mongoose.connect(dbURI, {server:{auto_reconnect:true}});
 
-var db = mongoose.connection;
 
-db.on('connecting', function() {
-  console.log('connecting to MongoDB...');
-});
-
-db.on('error', function(error) {
-  console.error('Error in MongoDb connection: ' + error);
-  mongoose.disconnect();
-});
-db.on('connected', function() {
-  console.log('MongoDB connected!');
-});
-db.once('open', function() {
-  console.log('MongoDB connection opened!');
-});
-db.on('reconnected', function () {
-  console.log('MongoDB reconnected!');
-});
-db.on('disconnected', function() {
-  console.log('MongoDB disconnected!');
-  mongoose.connect(dbURI, {server:{auto_reconnect:true}});
-});
-mongoose.connect(dbURI, {server:{auto_reconnect:true}});
-
-var contact = require('./models/contact');
-var conversation = require('./models/conversation');
-var conversation_reply = require('./models/conversation_reply');
+var Contact = require('./models/contact');
+var Conversation = require('./models/conversation');
+var Conversation_reply = require('./models/conversation_reply');
 
 // ROUTES
 // =============================================================================
@@ -71,18 +75,20 @@ router.route('/contact')
 
     var contact = new Contact();      // create a new instance of the contact model
     contact.id = req.body.id;
-    console.log(req.body);
     contact.name = req.body.name; // set the contact name (comes from the request)
+    console.log(contact);
+    console.log(contact.save);
 
     // save the contact and check for errors
     contact.save(function(err) {
+      console.log(req.body);
       if (err)
-      res.send(err);
+        return res.send(err);
 
       res.json({ message: 'Contact created!' });
     });
 
-  });
+  })
 
   // get all the contacts (accessed at GET http://localhost:3000/api/contact)
   .get(function(req, res) {
